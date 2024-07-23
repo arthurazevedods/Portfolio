7c417fa44a5b72452cb9cf566871761ad0b6ec4a
arthur <arthurazevedods@gmail.com>
2024-07-22 22:56:31 -0300

    Pages and tailwind

---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+
+</script>
+<template>
+    <div class="h-full">
+        <header class="hero h-1/4 bg-cover bg-center">
+            <nav class="shadow-blue-600bg-primary bg-primary bg-opacity-40 rounded-2x1 flex flex-row justify-between h-10">
+                <div>logo</div>
+                <ul class="flex items-center justify-around font-bold">
+                    <li>Início</li>
+                    <li>Blog</li>
+                    <li>Portfolio</li>
+                    <li>Contato</li>
+                </ul>
+            </nav>
+        </header>
+        <main>
+
+        </main>
+        <footer class="sticky top-full flex flex-row items-center text-gray-600 border-t-2 border-gray-600">
+            <img src="" alt="">
+            <p class="text-sm">
+                2024 Arthur Silva - 
+                <a href="" class="text-blue">@arthurazevedods</a>
+            </p>
+            <div class="ml-auto flex justify-center">icon</div>
+            <div class="ml-auto flex justify-center">icon</div>
+            <div class="ml-auto flex justify-center">icon</div>
+
+        </footer>
+    </div>
+</template>
+
+<style scoped>
+.hero{
+    background-image: url('/assets/images/teclado-nav-banner.jpg');
+    color: black;
+}
+</style>
